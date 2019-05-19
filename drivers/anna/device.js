07b492e6cfd8cf504911265b8bad5b578274aeb8
Jorrit Pouw <jorrit@synegic.nl>
2019-05-19 16:36:40 +0200

    implement measure_luminance, alarm_motion, location_preset capabilities in Anna
    also allow setting DHW_mode and location_preset from Flowcards

--- drivers/anna/device.js
+++ drivers/anna/device.js
@@ -4,20 +4,31 @@ const Homey = require('homey');
 const PlugwiseThermostatDevice = require('../../lib/PlugwiseThermostatDevice');
 
 module.exports = class PlugwiseAnnaDevice extends PlugwiseThermostatDevice {
-  
-  onInit(...props) {
-    super.onInit(...props);   
-  	
-    this.registerCapabilityListener('location_preset', this.onCapabilityLocationPreset.bind(this));
-  }
 
-  async onCapabilityLocationPreset( value ) {
-    const { applianceId } = this;
-        
-    return this.bridge.setPreset({
-      applianceId,
-      preset: value,
-    });
-  }
-	
-}
+    onInit(...props) {
+        super.onInit(...props);
+
+        this.registerCapabilityListener('location_preset', this.onCapabilityLocationPreset.bind(this));
+
+        this._setLocationPresetAction = new Homey.FlowCardAction('Set_Location_Preset')
+            .register().registerRunListener(this._setLocationPresetRunListener.bind(this));
+    }
+
+    async _setLocationPresetRunListener(args) {
+        const { applianceId } = this;
+
+        return args.device.bridge.setPreset({
+            applianceId: applianceId,
+            preset: args.preset
+        });
+    }
+
+    async onCapabilityLocationPreset(value) {
+        const { applianceId } = this;
+
+        return this.bridge.setPreset({
+            applianceId,
+            preset: value
+        });
+    }
+};
